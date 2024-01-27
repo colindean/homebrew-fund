159fbaa5937138ba1db01b6b5fef4a0b161b31a4
Colin Dean <colindean@users.noreply.github.com>
2024-01-27 18:13:52 +0000

    Move formula data extraction to the formula loader

--- lib/fund/formula_loader.rb
+++ lib/fund/formula_loader.rb
@@ -8,4 +8,13 @@ module FormulaLoader
   def self.get_formula(name)
     Formula[name]
   end
+
+  sig { params(formula: Formula).returns(T::Hash[Symbol, String]) }
+  def self.collect_data(formula)
+    {
+      homepage:   formula.homepage,
+      stable_url: formula.stable&.url,
+      head_url:   formula.head&.url,
+    }
+  end
 end
--- lib/fund/lookup_methods_resolver.rb
+++ lib/fund/lookup_methods_resolver.rb
@@ -22,21 +22,12 @@ class LookupMethodsResolver
 
   sig { params(formula: Formula).returns(T::Hash[String, LookupMethodBase]) }
   def resolve(formula)
-    data = collect_data(formula)
+    data = FormulaLoader.collect_data(formula)
 
     data.transform_values do |url|
       installed_lookup_methods.find do |method|
         method.try_from(url) # &.execute_query
       end
     end
-  end
-
-  sig { params(formula: Formula).returns(T::Hash[Symbol, String]) }
-  def collect_data(formula)
-    {
-      homepage:   formula.homepage,
-      stable_url: formula.stable&.url,
-      head_url:   formula.head&.url,
-    }
   end
 end
