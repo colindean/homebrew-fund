e2ddc31f0a6c363a495487b2accffb234bca451b
Colin Dean <colindean@users.noreply.github.com>
2024-01-28 07:33:22 +0000

    Drop usage of HashWithIndifferentAccess since it's not always available
    It seems to be available to the brew inside my devcontainer but not on
    my system. Odd.

--- lib/fund/lookup_methods/static_names_lookup.rb
+++ lib/fund/lookup_methods/static_names_lookup.rb
@@ -1,11 +1,9 @@
 # typed: true
 # frozen_string_literal: true
 
-require "active_support/core_ext/hash/indifferent_access"
-
 LOOKUP_TABLE = { brew:            GitHubSponsorsLookup.new("homebrew", ".github"),
                  "homebrew-fund": GitHubSponsorsLookup.new("colindean",
-                                                           "homebrew-fund") }.with_indifferent_access.freeze
+                                                           "homebrew-fund") }.freeze
 
 class StaticNamesLookup < LookupMethodBase
   extend ViableMethod
@@ -15,7 +13,8 @@ class StaticNamesLookup < LookupMethodBase
 
   sig { params(name: T.nilable(String)).returns(T.nilable(StaticNamesLookup)) }
   def self.try_from(name)
-    return StaticNamesLookup.new(name) if LOOKUP_TABLE.key?(name)
+    return if name.nil?
+    return StaticNamesLookup.new(name) if [name, name.to_sym].any? { |n| LOOKUP_TABLE.key?(n) }
 
     nil
   end
@@ -36,7 +35,7 @@ class StaticNamesLookup < LookupMethodBase
   end
 
   def execute
-    thing = LOOKUP_TABLE.fetch(name)
+    thing = LOOKUP_TABLE.fetch(name, LOOKUP_TABLE.fetch(name.to_sym))
     if thing.respond_to?(:execute)
       thing.execute
     else
