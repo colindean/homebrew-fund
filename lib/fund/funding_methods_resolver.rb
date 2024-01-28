9db73e86774e582f19fee22ed184e2734d9cda08
Colin Dean <colindean@users.noreply.github.com>
2024-01-28 02:36:42 +0000

    Clean up code a little more

--- lib/fund/funding_methods_resolver.rb
+++ lib/fund/funding_methods_resolver.rb
@@ -12,11 +12,14 @@ module FundingMethodsResolver
 
     methods = LookupMethodsResolver.instance.resolve(formula)
     by_viability = group_by_viability(methods)
-    if by_viability.has_key? false
-      odebug "#{by_viability[false].size} funding lookup methods non-viable for #{formula}: #{by_viability[false].keys}"
+    if by_viability.key? false
+      nopes = by_viability[false]
+      odebug "#{nopes.size} funding lookup methods non-viable for #{formula}: #{nopes.keys}"
     end
-    failure_message = "Try looking at its homepage, open it with 'brew homepage #{formula}' or look at 'brew info #{formula}' for other information."
-    unless by_viability.has_key? true
+    failure_message = <<~EOS
+      Try looking at its homepage, open it with 'brew homepage #{formula}' or look at 'brew info #{formula}' for other information.
+    EOS
+    unless by_viability.key? true
       onoe "No viable funding lookup methods available for #{formula}"
       return failure_message
     end
